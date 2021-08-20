dc70f94830e4573e761e0e09f3f667fa925fd5f3
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-08-19 21:50:52 -0700

    Import TopCards Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./style.css";
 import TopBar from './components/TopBar.js';
 import LineChart from './components/LineChart';
 import Cards from './components/Cards.js';
+import TopCards from './components/TopCards';
 
 export default function App() {
   return (
     <div>
       <TopBar />
       <h1>Global Times CMS</h1>
+      <h2>The Top-Three Best Performing Stories (All-Time)</h2>
+      <TopCards />
       <LineChart />
       <h2>The Latest Social Media Analytics</h2>
       <Cards />
