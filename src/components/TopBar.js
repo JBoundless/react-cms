f6d032404d5df2980940f87e36f699c321cc8dc9
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-08-19 21:36:37 -0700

    Update TopBar.js

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -56,7 +56,7 @@ export default function MenuAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            GitHub Times CMS
+            Global Times CMS
           </Typography>
           {auth && (
             <div>
